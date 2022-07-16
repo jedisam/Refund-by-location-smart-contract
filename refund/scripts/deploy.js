1dfa938fc9b8bd95dc7e84d8399d88c3ae0bb0e5
Yididiya <yidisam18@gmail.com>
2022-07-16 09:11:15 +0300

    smart contract deloyment with verification

--- refund/scripts/deploy.js
+++ refund/scripts/deploy.js
@@ -8,6 +8,26 @@ async function main() {
   const refund = await Refund.deploy();
   await refund.deployed();
   console.log("Refund contract deployed at:", refund.address);
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await refund.deployTransaction.wait(6);
+    await verify(refund.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Contract already verified");
+    } else {
+      console.log(error);
+    }
+  }
 }
 
 // main
